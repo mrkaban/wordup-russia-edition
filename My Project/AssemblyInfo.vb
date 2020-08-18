5401bc0901366a917278c254c94325aeaa64315c
Алексей <s-r-grass@mail.ru>
2020-08-18 22:33:39 +0700

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,51 @@
+﻿'This file is part of WordUp.
+
+'WordUp is free software: you can redistribute it and/or modify
+'it under the terms of the GNU General Public License as published by
+'the Free Software Foundation, either version 3 of the License, or
+'(at your option) any later version.
+
+'WordUp is distributed in the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty of
+'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License for more details.
+
+'You should have received a copy of the GNU General Public License
+'along with WordUp.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WordUp")> 
+<Assembly: AssemblyDescription("A free, familiar-looking alternative to Office Word.")> 
+<Assembly: AssemblyCompany("WordUp Open Source Developers")> 
+<Assembly: AssemblyProduct("WordUp")> 
+<Assembly: AssemblyCopyright("Copyright ©  2008 WordUp Open Source Developers.")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b9a923f4-d564-4605-b5d5-e80eb8df6bb2")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.0.4.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
